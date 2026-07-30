f4a0e4f05e30fc85c47ccd98c1e053b5e99ffb45
Travis Reed <dancxjo@users.noreply.github.com>
2026-07-30 13:17:22 -0700

    Publish the checked library catalog (#180)

--- xtask/src/commands/check_all.rs
+++ xtask/src/commands/check_all.rs
@@ -49,6 +49,12 @@ pub fn run(workspace_root: &Path) -> Result<(), Box<dyn std::error::Error>> {
     )?;
 
     run_step(
+        "library catalog index gate",
+        &["cargo", "xtask", "catalog-index", "--check"],
+        workspace_root,
+    )?;
+
+    run_step(
         "cargo check conduit-core thumbv6m-none-eabi",
         &[
             "cargo",
