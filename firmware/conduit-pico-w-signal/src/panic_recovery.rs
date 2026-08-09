30fe82497ffd196c5ec73f5650fa5a8d64337726
Travis Reed <tdreed@gmail.com>
2026-08-09 03:31:42 -0700

    Make Pico panic phase recording atomic

--- firmware/conduit-pico-w-signal/src/panic_recovery.rs
+++ firmware/conduit-pico-w-signal/src/panic_recovery.rs
@@ -7,7 +7,8 @@ use embassy_rp::{
     Peri,
 };
 
-const RECORD_MAGIC: u32 = 0x434e_4450;
+const RECORD_MAGIC: u32 = 0x434e_4400;
+const RECORD_MAGIC_MASK: u32 = 0xffff_ff00;
 
 #[derive(Clone, Copy)]
 #[repr(u32)]
@@ -33,28 +34,26 @@ impl PanicRecord {
 
 pub fn set_phase(phase: PanicPhase) {
     let watchdog = rp_pac::WATCHDOG;
-    watchdog.scratch0().write(|value| *value = RECORD_MAGIC);
-    watchdog.scratch1().write(|value| *value = phase as u32);
+    watchdog
+        .scratch0()
+        .write(|value| *value = RECORD_MAGIC | phase as u32);
 }
 
 pub fn clear() {
     let watchdog = rp_pac::WATCHDOG;
     watchdog.scratch0().write(|value| *value = 0);
-    watchdog.scratch1().write(|value| *value = 0);
 }
 
 pub fn take(watchdog_peripheral: Peri<'static, WATCHDOG>) -> Option<PanicRecord> {
     let mut watchdog = Watchdog::new(watchdog_peripheral);
     let forced_reset = watchdog.reset_reason() == Some(ResetReason::Forced);
-    let magic = watchdog.get_scratch(0);
-    let phase = watchdog.get_scratch(1);
+    let record = watchdog.get_scratch(0);
     watchdog.set_scratch(0, 0);
-    watchdog.set_scratch(1, 0);
-    if !forced_reset || magic != RECORD_MAGIC {
+    if !forced_reset || record & RECORD_MAGIC_MASK != RECORD_MAGIC {
         return None;
     }
     Some(PanicRecord {
-        phase: match phase {
+        phase: match record & !RECORD_MAGIC_MASK {
             1 => PanicPhase::RadioDriverStartup,
             2 => PanicPhase::RadioInitialization,
             _ => PanicPhase::Unclassified,
@@ -65,7 +64,7 @@ pub fn take(watchdog_peripheral: Peri<'static, WATCHDOG>) -> Option<PanicRecord>
 #[panic_handler]
 fn panic(_info: &PanicInfo<'_>) -> ! {
     let watchdog = rp_pac::WATCHDOG;
-    if watchdog.scratch0().read() != RECORD_MAGIC {
+    if watchdog.scratch0().read() & RECORD_MAGIC_MASK != RECORD_MAGIC {
         set_phase(PanicPhase::Unclassified);
     }
     rp_pac::PSM
