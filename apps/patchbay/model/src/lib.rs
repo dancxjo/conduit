55063e66b891bbe97b8dc1884b80bd626976e1bb
Travis Reed <dancxjo@users.noreply.github.com>
2026-08-31 22:36:13 -0700

    Project Body Signs as a readable biography (#2034)

--- apps/patchbay/model/src/lib.rs
+++ apps/patchbay/model/src/lib.rs
@@ -67,6 +67,7 @@ mod presenter_plans;
 mod presenter_plans_tests;
 mod prewake;
 pub mod proof;
+mod readable_body_history;
 mod recursive_recovery_explanation;
 mod renderer_conformance;
 mod renderer_execution;
@@ -210,6 +211,11 @@ pub use presentation_layout::{
 };
 pub use presenter_plans::*;
 pub use prewake::*;
+pub use readable_body_history::{
+    BodyHistoryAccess, BodyHistoryEntry, BodyHistoryExactEvidence, BodyHistoryInspectTarget,
+    BodyHistoryManifestation, BodyHistoryMoment, ReadableBodyHistory, ReadableBodyHistoryError,
+    ReadableBodyHistorySlot, MAX_BODY_HISTORY_LINEAR_BYTES, MAX_BODY_HISTORY_TITLE_BYTES,
+};
 pub use recursive_recovery_explanation::{
     explain_recursive_recovery, RecursiveRecoveryExplanation, RecursiveRecoveryExplanationError,
     MAX_RECURSIVE_RECOVERY_EXPLANATION_BYTES,
