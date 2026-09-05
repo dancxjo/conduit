032ac39e7b7875a14fd6f64efd148c8f2012189c
Travis Reed <tdreed@gmail.com>
2026-09-04 20:17:55 -0700

    test: harden bounded replay inputs

--- semantics/time/tests/replay_codec.rs
+++ semantics/time/tests/replay_codec.rs
@@ -59,6 +59,21 @@ fn output_truncation_magic_version_utf8_and_trailing_fail_distinctly() {
         decode_replay_timeline(&malformed),
         Err(ReplayTimelineCodecRefusal::TrailingBytes)
     );
+
+    malformed = encoded[..length].to_vec();
+    malformed.truncate(9);
+    assert_eq!(
+        decode_replay_timeline(&malformed),
+        Err(ReplayTimelineCodecRefusal::Truncated)
+    );
+
+    malformed = encoded[..length].to_vec();
+    let first_identity_length = usize::from(u16::from_le_bytes([malformed[7], malformed[8]]));
+    malformed.truncate(9 + first_identity_length + 7);
+    assert_eq!(
+        decode_replay_timeline(&malformed),
+        Err(ReplayTimelineCodecRefusal::Truncated)
+    );
 }
 
 #[test]
@@ -105,5 +120,40 @@ fn empty_duplicate_reordered_and_oversized_timelines_refuse() {
     assert_eq!(
         encode_replay_timeline_into(&too_many, &mut output),
         Err(ReplayTimelineCodecRefusal::TooManyEntries)
+    );
+
+    let empty_identity = vec![HistoricalReplayEntry {
+        identity: String::new(),
+        event_ticks: 0,
+    }];
+    assert_eq!(
+        encode_replay_timeline_into(&empty_identity, &mut output),
+        Err(ReplayTimelineCodecRefusal::EmptyIdentity)
+    );
+    let long_identity = vec![HistoricalReplayEntry {
+        identity: "x".repeat(MAXIMUM_REPLAY_IDENTITY_BYTES + 1),
+        event_ticks: 0,
+    }];
+    assert_eq!(
+        encode_replay_timeline_into(&long_identity, &mut output),
+        Err(ReplayTimelineCodecRefusal::IdentityTooLong)
+    );
+
+    let mut declared_too_many = vec![0_u8; 7];
+    declared_too_many[..4].copy_from_slice(b"CRTL");
+    declared_too_many[4] = REPLAY_TIMELINE_WIRE_VERSION;
+    declared_too_many[5..7].copy_from_slice(&((MAXIMUM_REPLAY_ENTRIES + 1) as u16).to_le_bytes());
+    assert_eq!(
+        decode_replay_timeline(&declared_too_many),
+        Err(ReplayTimelineCodecRefusal::TooManyEntries)
+    );
+
+    let mut declared_empty_identity = vec![0_u8; 9];
+    declared_empty_identity[..4].copy_from_slice(b"CRTL");
+    declared_empty_identity[4] = REPLAY_TIMELINE_WIRE_VERSION;
+    declared_empty_identity[5..7].copy_from_slice(&1_u16.to_le_bytes());
+    assert_eq!(
+        decode_replay_timeline(&declared_empty_identity),
+        Err(ReplayTimelineCodecRefusal::EmptyIdentity)
     );
 }
