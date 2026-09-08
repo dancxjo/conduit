ce02fe29f3c524aa484587eff9d0d716131433c6
Travis Reed <dancxjo@users.noreply.github.com>
2026-09-07 22:51:15 -0700

    Repair ConduitOS compositor arena admission (#3014)

--- targets/conduitos/fabrication/xtask/run.rs
+++ targets/conduitos/fabrication/xtask/run.rs
@@ -484,7 +484,7 @@ fn validate_boot(sign: &GuestBootSign) -> Result<(), ConduitosError> {
         || sign.host_id.len() != 64
         || sign.boot_id.len() != 64
         || sign.memory_regions == 0
-        || sign.runtime_arena_bytes != 4_194_304
+        || sign.runtime_arena_bytes != 8_388_608
     {
         return Err(ConduitosError::refusal(
             "invalid-boot-sign",
