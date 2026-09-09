7d571b86fd3c15986dab547f5118b17b3cb464d5
Travis Reed <dancxjo@users.noreply.github.com>
2026-09-09 15:06:46 -0700

    Bind keyboard proof capacities to HID realization

--- targets/conduitos/fabrication/xtask/keyboard_run.rs
+++ targets/conduitos/fabrication/xtask/keyboard_run.rs
@@ -73,8 +73,8 @@ pub(super) fn validate(
         || keyboard.plan_id == keyboard.active_play_id
         || keyboard.resource_bindings != 8
         || keyboard.report_buffers != u16::from(hid.report_buffers)
-        || keyboard.transition_slots != 8
-        || keyboard.operation_slots != 2
+        || keyboard.transition_slots != u16::from(hid.sign_slots)
+        || keyboard.operation_slots != u16::from(hid.maximum_outstanding_interrupt_transfers)
         || keyboard.cord_item_capacity != 1
         || keyboard.cord_byte_capacity != 3
         || keyboard.event_count != 2
