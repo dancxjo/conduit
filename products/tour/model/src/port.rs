5022739e91eaccdf07c442300ec57d82386af7fa
Travis Reed <tdreed@gmail.com>
2026-09-13 09:08:08 -0700

    Prove Tour through the reviewed Form inventory

--- products/tour/model/src/port.rs
+++ products/tour/model/src/port.rs
@@ -70,22 +70,6 @@ mod tests {
     use crate::OPEN_PATCHBAY_ACTION_ID;
     use conduit_presentation::ApplicationEventKind;
 
-    const FORM_SOURCE: &str = include_str!("../../../../forms/tour/main.conduit");
-
-    #[test]
-    fn tour_is_a_checked_host_neutral_form_over_the_application_seam() {
-        let mut startup = conduit_form::StartupCatalog::new();
-        let mut profile = conduit_form::ProfileCatalog::new();
-        conduit_semantic_catalog::install_application_catalogs(&mut startup, &mut profile).unwrap();
-        let syntax = conduit_form::parse_syntax_document(FORM_SOURCE);
-        let checked = conduit_form::check_syntax_document(&syntax, &startup).unwrap();
-        let expanded = conduit_form::expand_canonical_form(&checked, "tour", &profile).unwrap();
-        assert_eq!(expanded.gears.len(), 3);
-        assert_eq!(expanded.connections.len(), 2);
-        assert!(!FORM_SOURCE.contains("Host"));
-        assert!(!FORM_SOURCE.contains("implementation"));
-    }
-
     #[test]
     fn initial_view_and_real_event_share_the_typed_port_encoding() {
         let mut port = TourApplicationPort::canonical();
