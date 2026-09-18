1d2476183152f6909cf50ec7c520a357d9d3127b
Travis Reed <dancxjo@users.noreply.github.com>
2026-09-18 10:15:41 -0700

    tongues: re-export recognition event identity types

--- semantics/tongues/src/streaming_recognition.rs
+++ semantics/tongues/src/streaming_recognition.rs
@@ -13,6 +13,7 @@ use std::{string::String, vec, vec::Vec};
 
 use crate::{SpeechRecognitionContract, MAXIMUM_RECOGNIZED_TEXT_BYTES};
 
+pub use speaking::{SegmentId, StreamEvent, TextRole};
 pub use speaking::StreamEvent as RecognitionEvent;
 
 pub const STREAMING_SPEECH_RECOGNIZE_KIND: &str = "speech/recognize-stream";
